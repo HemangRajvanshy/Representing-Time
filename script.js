fa9ecd0500d9e487d60434fb99e50cfb62581864
HemangRajvanshy <hemangrajvanshy@gmail.com>
2018-10-02 14:16:43 -0400

    Button Disable

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 var counter = 0;
 var time;
+
 function buttonPress()  {
+    var Button = document.getElementById("btn");
+    var CountText = document.getElementById("counter");
+
     if (counter == 0)   {
-        var d = new Date();
-        time = d.getTime();
+        time = (new Date()).getTime();
         document.getElementById("intro").innerHTML = "Press the button below 20 times at what you <br> perceive    to be  roughly one second interval. <br> <br> <br>";
         document.getElementById("buffer").insertAdjacentHTML("afterbegin", "<span style=\"display:block; height: 0em;\"></span>");
-
+        Button.innerHTML = "Click"
     }
 
     if (counter == 20)  { //when the counter was 19 and I pressed it for the 20th time
-        var d = new Date();
-        var diff = d.getTime() - time;
-        document.getElementById("counter").innerHTML = diff/1000;
-
+        var diff = (new Date()).getTime() - time;
+        CountText.innerHTML = diff/1000;
+        Button.disabled = true;
+        Button.innerHTML = "Stop!";
     }   else    {
-        document.getElementById("counter").innerHTML = counter;
+        CountText.innerHTML = counter;
         counter++;
     }
 }
