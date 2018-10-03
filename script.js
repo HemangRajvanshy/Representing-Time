7a95423be3c463e3ee4e89b9204aade0e958fecc
HemangRajvanshy <hemangrajvanshy@gmail.com>
2018-10-02 22:35:51 -0400

    Complete!

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var counter = 0;
 var time;
 var CountText, Button;
 
+document.getElementById("reload").style.display = "none";
+
 function buttonPress()  {
     Button = document.getElementById("btn");
     CountText = document.getElementById("counter");
@@ -25,12 +27,20 @@ function init() {
 }
 
 function end()  {
+//    document.getElementById("intro").style.display = "none";
+    document.getElementById("reload").style.display = "block";
+    $("#intro").fadeOut("slow");
+    $("#counter").fadeOut("slow");
     var diff = (new Date()).getTime() - time;
     CountText.style.fontSize = "x-large";
-    CountText.innerHTML = "You were off by " + Math.abs(Number(20 - diff/1000).toFixed(3)) + " seconds";
+    CountText.innerHTML = "<br> You were off by " + Math.abs(Number(20 - diff/1000).toFixed(3)) + " seconds";
     CountText.innerHTML += "<br> <br> What you thought to be 20 seconds were actually " + Number(diff/1000).toFixed(3) + " seconds. <br>"
     CountText.innerHTML += "Research suggests that our sense of time (or chronoception) is closely related to your age." +
-                                "writintg for tessdfja dkfajesf dkljsf sdlkfj esdlfk j";
+                                "Older adults perceive time as passing at a relatively fast rate because the biological clock slows progressively"
+                                + "as a person ages. The slowing of a biological clock presumably " +
+                                "gives rise to the feeling that external time is passing faster.";
+
+    $("#counter").fadeIn("slow");
     Button.disabled = true;
     Button.innerHTML = "Stop!";
 }
